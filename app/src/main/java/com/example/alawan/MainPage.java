20086ea9abe444df978b4b4a0c08db0bf6bbfcf3
thony311 <antoinelefebvrepoupa@hotmail.com>
2024-02-14 17:39:08 -0500

    debut du bottom menu
    Voici le debut du menu en dessou. J'essaie encore de me retrouver. finalement la video que javais trouver était obselete

--- app/src/main/java/com/example/alawan/MainPage.java
+++ app/src/main/java/com/example/alawan/MainPage.java
@@ -1,14 +1,37 @@
 package com.example.alawan;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity
+        //implements BottomNavigationView.OnNavigationItemSelectedListener
+    {
 
+    BottomNavigationView bottomMenu;
+    Map map = new Map();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        //bottomMenu = findViewById(R.id.bottomMenu);
+        //bottomMenu.setOnNavigationItemReselectedListener(this);
     }
+
+/*
+    @Override // C'est se qui permet de naviguer avec le menu
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.mapMenu:getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, map).commit();return true;
+            case R.id.rechercheMenu:return true;
+
+        }
+
+        return false;
+    }*/
+
 }
