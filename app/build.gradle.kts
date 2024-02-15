cb47bf4b5f268af430424d0d403ac5b8d8be1468
thony311 <antoinelefebvrepoupa@hotmail.com>
2024-02-15 14:27:56 -0500

    push navbar fait
    La nav bar est fait. Il manquera juste le code ou des modification pour zak

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     var nav_version = "2.5.3"
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
