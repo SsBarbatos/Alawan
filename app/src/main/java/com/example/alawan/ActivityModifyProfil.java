72af1ea1fe09820c901457760962ae5ba52826e2
thony311 <antoinelefebvrepoupa@hotmail.com>
2024-02-28 12:30:38 -0500

    push to merge

---
+++ app/src/main/java/com/example/alawan/ActivityModifyProfil.java
@@ -0,0 +1,14 @@
+package com.example.alawan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityModifyProfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_profil);
+    }
+}
--- app/src/main/java/com/example/alawan/Profil.java
+++ app/src/main/java/com/example/alawan/Profil.java
@@ -7,18 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
 
 
 public class Profil extends Fragment {
 
 
-
+    ImageView ivSetting;
+    View view;
     public Profil() {
         // Required empty public constructor
     }
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,9 +26,9 @@ public class Profil extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_profil, container, false);
+
+        return view;
     }
 }
